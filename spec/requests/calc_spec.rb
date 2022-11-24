fad81a96d1488c91f1d0ba997cce49b411eaacdd
MrDzhofik <djafarovemil04@mail.ru>
2022-11-24 12:04:56 +0300

    Right test was done

--- spec/requests/calc_spec.rb
+++ spec/requests/calc_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe "Calcs", type: :request do
       get "/calc/show"
       expect(response).to have_http_status(:success)
     end
+
+    it 'return right values' do
+      get "/calc/show", params: { :power => "3"}
+      expect(assigns(:right_numbers)).to eq([153, 370, 371, 407])
+    end
   end
 
 end
